4ad71ea82885cee2ef5f358ef59737d8869b722c
rsngn <henri.prasetya@ti.ukdw.ac.id>
2022-05-22 21:01:54 +0700

    init laravel 9

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Landing Page</title>
+
+        <!-- Fonts -->
+        <link href="" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="/css/app.css" rel="stylesheet">
+        </style>
+    </head>
+    <body>
+        <div>
+        </div>
+    </body>
+</html>
